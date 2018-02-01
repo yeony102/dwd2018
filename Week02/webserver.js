25fddc99cc61fa2e7fb9745db9ba83a2a0aa3884
yeony102 <signup.yeonhee.lee@gmail.com>
2018-02-01 16:01:14 -0500

    updated for formpost testing

--- Week02/webserver.js
+++ Week02/webserver.js
@@ -8,9 +8,18 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.get('/somethingelse', function (req, res) {
+  res.send('<html><body><h1>Someting Else</h1></body></html>');
+})
+
 app.get('/randomfile', function (req, res) {
 	var fileToSend = "somerandomfile.txt";
 	res.sendfile(fileToSend, {root: './public'}); // Files inside "public" folder
+})
+
+app.get('/formpost', function (req, res) {
+	var textValue = req.query.textfield;
+	res.send("You submitted: " + textValue);
 })
 
 app.listen(3000, function () {
