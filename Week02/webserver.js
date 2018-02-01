0e16d31135ab35cbcd87176386f4fe240817c282
yeony102 <signup.yeonhee.lee@gmail.com>
2018-02-01 15:36:06 -0500

    updated webserber.js and removed somerandomfile.txt

--- Week02/webserver.js
+++ Week02/webserver.js
@@ -1,8 +1,16 @@
 var express = require('express')
 var app = express()
 
+
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
+})
+
+app.get('/randomfile', function (req, res) {
+	var fileToSend = "somerandomfile.txt";
+	res.sendfile(fileToSend, {root: './public'}); // Files inside "public" folder
 })
 
 app.listen(3000, function () {
