369f2741ddea09262e54cc95269470f6d4b803d6
yeony102 <signup.yeonhee.lee@gmail.com>
2018-02-01 15:32:15 -0500

    updated webserver.js

--- Week02/webserver.js
+++ Week02/webserver.js
@@ -1,16 +1,14 @@
 var express = require('express')
 var app = express()
 
-app.use(express.static('public'));
-
 app.get('/', function (req, res) {
   res.send('Hello World!')
-})
-
-app.get('/somethingelse', function (req, res) {
-	res.send('<html><body><h1>Something Else</h1></body></html>')
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+
+
+
