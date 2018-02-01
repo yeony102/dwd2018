3404633d92541c3cb460af9a1719854bef68bec2
yeony102 <signup.yeonhee.lee@gmail.com>
2018-02-01 15:21:36 -0500

    updated webserver.js

--- Week02/webserver.js
+++ Week02/webserver.js
@@ -1,12 +1,19 @@
 var express = require('express')
 var app = express()
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
 app.get('/somethingelse', function (req, res) {
 	res.send('<html><body><h1>Something Else</h1></body></html>')
+})
+
+app.get('/randomfile', function (req, res) {
+	var fileToSend = "somerandomfile.txt";
+	res.sendfile(fileToSend, {root: './public'}); 
 })
 
 app.listen(3000, function () {
