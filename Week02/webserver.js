aa1515054ee325844cfc1a4a753719d430005e12
yeony102 <signup.yeonhee.lee@gmail.com>
2018-02-01 14:44:09 -0500

    added server.js in Week01, created Week02

---
+++ Week02/webserver.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/somethingelse', function (req, res) {
+	res.send('<html><body><h1>Something Else</h1></body></html>')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
