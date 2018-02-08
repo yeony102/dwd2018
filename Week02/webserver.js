de823cc578c065409ecdcb2a092c58a5b1120936
yeony102 <signup.yeonhee.lee@gmail.com>
2018-02-08 00:35:01 -0500

    dwd w2 assignment

--- Week02/webserver.js
+++ Week02/webserver.js
@@ -9,33 +9,22 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+
+var smallHappiness = ["Scone with cream and jam", "", ""];
+
+app.get('/areyouhappy', function (req, res) {
+	var text = req.query.textfield;
+	var html = "<html><body><div style=\"width: 50%; height: 50%; margin: auto; text-align:center; font-family:san-serif;\"><h1>"
+	html = html + text + " makes you happy!</h1><p><br>Find out what other people answered</p><form method=\"GET\" action=\"/iamhappy\"><input type=\"submit\" name=\"showme\" value=\"Show me\" /></form></div></body></html>"
+	res.send(html);
+	smallHappiness.push(text);
 })
 
-app.get('/somethingelse', function (req, res) {
-  res.send('<html><body><h1>Someting Else</h1></body></html>');
-})
+app.get('/iamhappy', function (req, res) {
 
-app.get('/randomfile', function (req, res) {
-	var fileToSend = "somerandomfile.txt";
-	res.sendfile(fileToSend, {root: './public'}); // Files inside "public" folder
-})
-
-var archive = [];
-
-app.get('/formpost', function (req, res) {
-	var textValue = req.query.textfield;
-	res.send("You submitted: " + textValue);
-	archive.push(textValue);
-})
-
-app.get('/display', function (req, res) {
-	var html = "<html><body>";
-	for(var i=0; i<archive.length; i++) {
-		html = html + archive[i] + "<br>";
-	}
-	html = html + "</body></html>";
+	var i = Math.floor(Math.random(smallHappiness.length-1));
+	var html ="<html><body><div style=\"width: 50%; height: 50%; margin: auto; text-align:center; font-family:san-serif;\"><h1>"
+	html = html + smallHappiness[i] + " is someone's small happiness.</h1></div></body></html>"
 	res.send(html);
 })
 
