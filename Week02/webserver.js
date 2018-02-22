64e0fd28f6e7d21162a2aff69b8f1566cadeabce
Yeonhee Lee <signup.yeonhee.lee@gmail.com>
2018-02-22 05:39:21 +0000

    updated week02

--- Week02/webserver.js
+++ Week02/webserver.js
@@ -15,8 +15,16 @@ var smallHappiness = ["A Scone with Jam and Cream", "Honey Nut Cereal", "Belgian
 app.get('/areyouhappy', function (req, res) {
 	var text = req.query.textfield;
 	smallHappiness.push(text);
-	var fileToSend = "happy.html";
-	res.sendfile(fileToSend, {root: './public'});
+///	var fileToSend = "happy.html";
+//	res.sendfile(fileToSend, {root: './public'});
+	var html = "<html><body>";
+	for(var i=0; i<smallHappiness.length; i++) {
+		html = html + smallHappiness[i] + "<br>";
+	}
+	html = html + "<h1>Find out what other people answered</h1><form method=\"GET\" action=\"/iamhappy\"><input type=\"submit\" name=\"showme\" value=\"Show me\" />";
+	html = html + "</form>"
+	html = html + "</body></html>";
+	res.send(html);
 })
 
 app.get('/iamhappy', function (req, res) {
