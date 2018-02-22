eb8d2c6649e0c22e688e693eab9dbd47f5a48531
yeony102 <signup.yeonhee.lee@gmail.com>
2018-02-22 04:46:20 -0500

    assignment week03 is now working

--- Week03/server.js
+++ Week03/server.js
@@ -10,7 +10,7 @@ app.use(urlencodedParser);
 var config = require("./config.js");
 var mongojs = require('mongojs');
 //var db = mongojs(config.username+":"+config.password+"@ds217898.mlab.com:17898/dwd2018");
-var db = mongojs(config.username+":"+config.password+"@ds217898.mlab.com:17898/dwd2018", ['submissions']);
+var db = mongojs(config.username+":"+config.password+"@ds217898.mlab.com:17898/dwd2018", ["happythings"]);
 
 // ejs
 app.set('view engine', 'ejs');	// all render files(.ejs) are called from 'views' folder by default
@@ -18,35 +18,55 @@ app.set('view engine', 'ejs');	// all render files(.ejs) are called from 'views'
 
 app.use(express.static('public'));
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
 });
 
-var submissions_array = [];
-
-app.get('/formpost', function (req, res) {
-	var textValue = req.query.textfield;
-	res.send("You submitted: " + textValue);
-
-	db.submissions.save()({"textfield":textValue}, function(err, saved) {
-  		if( err || !saved ) console.log("Not saved");
-  		else console.log("Saved");
-  	});
-
-});
-
-
-app.get('/display', function(req, res) {
-	db.submissions.find({}, function(err, saved) {
-		if (err || !saved) {
-  			console.log("No results");
-	    }
-	    else {
-		  	saved.forEach( function(record) {
-		    	console.log(record);
-		    	//res.send(record);
-		    	//res.render('display.ejs', {submissions_on_page:submissions_array});
-		  	});
-	  	}
+app.get('/areyouhappy', function (req, res) {
+	var text = req.query.textfield;
+	
+	db.happythings.save({"happy":text}, function(err, saved) {
+		if( err || !saved ) console.log("Not saved");
+		else console.log("Saved");
 	});
-});
+
+	res.redirect('/youarehappy');
+})
+
+
+app.get('/youarehappy', function (req, res) {
+	db.happythings.find({}, function(err, saved) {
+		if(err || !saved) console.log("Not saved");
+		else {
+			console.log(saved);
+			res.render('happy.ejs', {dbdata:saved});
+		}
+	});
+})
+
+
+/*
+var smallHappiness = ["A Scone with Jam and Cream", "Honey Nut Cereal", "Belgian Beers"];
+
+app.get('/areyouhappy', function (req, res) {
+	var text = req.query.textfield;
+	smallHappiness.push(text);
+	var fileToSend = "happy.html";
+	res.sendfile(fileToSend, {root: './public'});
+})
+
+app.get('/iamhappy', function (req, res) {
+
+	var html = "<html><body><h1>";
+	for(var i=0; i<smallHappiness.length; i++) {
+		html = html + smallHappiness[i] + "<br>";
+	}
+	html = html + "</body></html>";
+	res.send(html);
+})
+*/
+
+
+
+
+
